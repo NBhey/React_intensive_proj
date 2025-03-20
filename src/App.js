8a004a9d31c97c61d0811f1edcbec88ebeb90dde
Nikita_Bondarev <raven1337@icloud.com>
2025-03-20 23:42:37 +0300

    validated form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import SignUpPage from './pages/SignUp/SignUp';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
 
 function App() {
   return (
