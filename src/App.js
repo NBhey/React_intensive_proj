f138eb33e8cb5d301cf683f92366ac9af2f7f2a7
Nikita_Bondarev <raven1337@icloud.com>
2025-03-22 17:56:28 +0300

    worked on SignUpPage&SignInPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
+import SignInPage from './pages/SignInPage/SignInPage';
 
 function App() {
   return (
     <div className="App">
       <SignUpPage/>
+      <SignInPage/>
     </div>
   );
 }
